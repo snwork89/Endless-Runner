f34cffa93b3aeb3836bd12457826d7bed79f294f
smitsabhadiya <smitsabhadiya7559@gmail.com>
2022-10-03 11:31:45 +0530

    prefavb_spawn

--- Assets/Scripts/MainObject.cs
+++ Assets/Scripts/MainObject.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class MainObject : MonoBehaviour
 {
     private bool firstLane=false,secondLane=true,thirdLane =false;
-
+    public float speed = 15.0f;
+    public GameObject spawnPrefab;
     void Update()
     {
         Vector3 movement = new Vector3(-1, 0, 0);
-        transform.Translate(movement * 5.0f * Time.deltaTime);
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        
+        if (((int)transform.position.x) % 300 == 0)
+        {
+            
+
+           
+            GameObject[] foundObjects = GameObject.FindGameObjectsWithTag("WorldPrefab");
+            
+            if (foundObjects.Length == 2)
+            {
+                Destroy(foundObjects[0]);
+            }
+
+            Instantiate(foundObjects[foundObjects.Length-1], new Vector3(transform.position.x + 60, 0, 0), transform.rotation);
+        }
+     
     }    
     
 
@@ -57,4 +75,6 @@ public class MainObject : MonoBehaviour
 
         }
     }
+  
+   
 }
